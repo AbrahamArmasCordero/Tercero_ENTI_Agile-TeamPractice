0f789c0d41cd7e18d81cd340733095e9bcd5d556
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-11-08 10:49:56 +0100

    Scenes Creation

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ var tetris = tetris || {};
 
 tetris.game = new Phaser.Game(128,256,Phaser.AUTO,null,this,false,false);
 
-tetris.game.state.add('mainMenu',tetris.mainMenu);
-tetris.game.state.add('inGame',tetris.inGame);
+tetris.game.state.add('mainMenu', tetris.mainMenu);
+tetris.game.state.add('inGame', tetris.inGame);
+tetris.game.state.add('ranking', tetris.ranking);
+tetris.game.state.add('endGame', tetris.endGame);
+
 tetris.game.state.start('mainMenu');
 
 
---
+++ js/ranking.js
@@ -0,0 +1,14 @@
+var tetris = tetris || {};
+
+
+tetris.ranking = {
+    
+    preload:function(){
+        //game.stage.backgroundColor = "#FF0000";
+    },
+    create:function(){
+    },
+    update:function(){
+        
+    }
+};
