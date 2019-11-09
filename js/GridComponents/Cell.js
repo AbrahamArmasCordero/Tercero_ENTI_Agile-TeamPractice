bf27d67e29ab9acd928ace4900b601e95ef9e7ac
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-11-09 19:26:15 +0100

    Grid Estructure

---
+++ js/GridComponents/Cell.js
@@ -0,0 +1,29 @@
+var tetris = tetris || {};
+
+const CellStates = {
+    EMPTY: 0,
+    PLACED: 1,
+    MOVING: 2,
+}
+
+const SpriteID ={
+    O: 0,
+    LR:{
+      Right: 1,
+      Left: 2
+    },
+    I: 3,
+    T: 4,
+    Z: {
+        Right: 5,
+        Left: 6
+    }
+}
+
+tetris.Cell = function(){
+    this.state = CellStates.EMPTY;
+    this.spriteID = SpriteID.O;
+};
+ 
+tetris.Cell.prototype = Object.create(tetris.Cell.prototype);
+tetris.Cell.prototype.constructor = tetris.Cell;
