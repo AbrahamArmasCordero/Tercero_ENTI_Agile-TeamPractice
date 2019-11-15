acd3e3e1ea8e5a283f62413b317cb684530c6e54
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-11-15 15:12:08 +0100

    Repeticion de codigo quitado

--- js/scenes/inGame.js
+++ js/scenes/inGame.js
@@ -25,73 +25,18 @@ tetris.inGame = {
         estate = 1;
         this.pj1GridBG = this.game.add.image(182,80, 'grid_bg');
         this.pj2GridBG = this.game.add.image(864,80, 'grid_bg');
+        
         this.inputHandler = new tetris.inputManager(tetris.game);
         
-        this.testGrid = new tetris.Grid(gameOptions.grid01PositionX,gameOptions.grid01PositionY);
-        this.testGrid.AddPiece(new tetris.lLPiece(), 1, 0);
-        this.testGrid.MovePiece(TypeOfMovement.LEFT);
+        this.player1 = new tetris.Player(this.inputHandler.cursorsPlayer01, gameOptions.grid01PositionX,gameOptions.grid01PositionY);
         
-        this.testGrid02 = new tetris.Grid(gameOptions.grid02PositionX, gameOptions.grid02PositionY);
-        this.testGrid02.AddPiece(new tetris.lLPiece(), 1, 0);
-        this.testGrid02.MovePiece(TypeOfMovement.LEFT);
+        this.player2 = new tetris.Player(this.inputHandler.cursorsPlayer02, gameOptions.grid02PositionX, gameOptions.grid02PositionY);
+
     },
     update:function(){
-        //LEFT
-        if(this.inputHandler.cursorsPlayer01.left.isDown && this.inputHandler.cursorsPlayer01.left.downDuration(1))
-        {
-            this.testGrid.MovePiece(TypeOfMovement.LEFT);
-        }
-        //RIGHT
-        else if(this.inputHandler.cursorsPlayer01.right.isDown && this.inputHandler.cursorsPlayer01.right.downDuration(1))
-        {
-            this.testGrid.MovePiece(TypeOfMovement.RIGHT);
-        }
-        //DOWN Independent
-        if(this.inputHandler.cursorsPlayer01.down.isDown && this.inputHandler.cursorsPlayer01.down.downDuration(1))
-        {
-            this.testGrid.MovePiece(TypeOfMovement.FASTER);
-        }
-        if(this.inputHandler.cursorsPlayer01.up.isDown && this.inputHandler.cursorsPlayer01.up.downDuration(1))
-        {
-            //this.testGrid.MovePiece(TypeOfMovement.DROP);
-        }
-        if(this.inputHandler.cursorsPlayer01.hold.isDown && this.inputHandler.cursorsPlayer01.hold.downDuration(1))
-        {
-            //this.testGrid.MovePiece(TypeOfMovement.FASTER);
-        }
-        if(this.inputHandler.cursorsPlayer01.rotate.isDown && this.inputHandler.cursorsPlayer01.rotate.downDuration(1))
-        {
-            this.testGrid.MovePiece(TypeOfMovement.ROTATE);
-        }
+        this.player1.TestFunc();
+        this.player2.TestFunc();
         
-        
-        
-        if(this.inputHandler.cursorsPlayer02.left.isDown && this.inputHandler.cursorsPlayer02.left.downDuration(1))
-        {
-            this.testGrid02.MovePiece(TypeOfMovement.LEFT);
-        }
-        //RIGHT
-        else if(this.inputHandler.cursorsPlayer02.right.isDown && this.inputHandler.cursorsPlayer02.right.downDuration(1))
-        {
-            this.testGrid02.MovePiece(TypeOfMovement.RIGHT);
-        }
-        //DOWN Independent
-        if(this.inputHandler.cursorsPlayer02.down.isDown && this.inputHandler.cursorsPlayer02.down.downDuration(1))
-        {
-            this.testGrid02.MovePiece(TypeOfMovement.FASTER);
-        }
-        if(this.inputHandler.cursorsPlayer02.up.isDown && this.inputHandler.cursorsPlayer02.up.downDuration(1))
-        {
-            //this.testGrid.MovePiece(TypeOfMovement.DROP);
-        }
-        if(this.inputHandler.cursorsPlayer02.hold.isDown && this.inputHandler.cursorsPlayer02.hold.downDuration(1))
-        {
-            //this.testGrid.MovePiece(TypeOfMovement.FASTER);
-        }
-        if(this.inputHandler.cursorsPlayer02.rotate.isDown && this.inputHandler.cursorsPlayer02.rotate.downDuration(1))
-        {
-            this.testGrid.MovePiece(TypeOfMovement.ROTATE);
-        }
     },
     
     play:function(){
