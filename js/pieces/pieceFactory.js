1e039be69ad2c5e5c919248df0a6c78bc2a0f237
Marc Baqués <marc.baques@enti.cat>
2019-11-30 21:33:40 +0100

    Factory Next Piece update

--- js/pieces/pieceFactory.js
+++ js/pieces/pieceFactory.js
@@ -4,6 +4,8 @@ tetris.pieceFactory = function(){
     //Historical of fieces
     //Max historical of pieces
     this.historicalPiece = [0,0];
+    this.nextPiece = tetris.game.rnd.integerInRange(0, 6);
+    this.createdPiece = 0;
 };
  
 
@@ -12,29 +14,29 @@ tetris.pieceFactory.prototype.constructor = tetris.pieceFactory;
 
 tetris.pieceFactory.prototype.createPiece = function(){
     //While not repeated 2 times on historical
-    var indPiece = 0;
+    this.createdPiece = this.nextPiece;
     do{
-        indPiece = tetris.game.rnd.integerInRange(0, 6);
-    }while(this.is2TimesRepeated(indPiece))
-        
+        this.nextPiece = tetris.game.rnd.integerInRange(0, 6);
+    }while(this.is2TimesRepeated(this.createdPiece))
+
     this.historicalPiece.shift();
-    this.historicalPiece.push(indPiece);
+    this.historicalPiece.push(this.createdPiece);
     
-    switch(indPiece){  
+    switch(this.createdPiece){  
         case 0:
-            return new tetris.iPiece();
-        case 1:
-            return new tetris.lLPiece();
-        case 2:
-            return new tetris.lRPiece();
-        case 3:
             return new tetris.oPiece();
+        case 1:
+            return new tetris.lRPiece();
+        case 2:
+            return new tetris.lLPiece();
+        case 3:
+            return new tetris.iPiece();
         case 4:
             return new tetris.tPiece();
         case 5:
-            return new tetris.zLPiece();
-        case 6:
             return new tetris.zRPiece();
+        case 6:
+            return new tetris.zLPiece();
     }
 }
 tetris.pieceFactory.prototype.is2TimesRepeated = function(numberPieceIndex){
