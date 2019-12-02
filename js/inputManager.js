7c222fc8229494878ccc17c89e1818064e9ff796
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-12-02 17:21:12 +0100

    Input Changes

--- js/inputManager.js
+++ js/inputManager.js
@@ -3,24 +3,24 @@ var tetris = tetris || {};
 tetris.inputManager = function(_game){
     //Inputs for Player 02
     this.cursorsPlayer02 = _game.input.keyboard.addKeys(
-            { 'up': Phaser.KeyCode.UP, 
+            { 'up': Phaser.KeyCode.NUMPAD_0, 
              'down': Phaser.KeyCode.DOWN, 
              'left': Phaser.KeyCode.LEFT, 
              'right': Phaser.KeyCode.RIGHT,
              'hold': Phaser.KeyCode.CONTROL,
-             'rotate': Phaser.KeyCode.NUMPAD_0});
-        //Keypad 0 = Rotate / CTRL 2 = Hold
+             'rotate': Phaser.KeyCode.UP});
+        //Keypad UP = Rotate / CTRL 2 = Hold
 
     
     //Input for Player 01
     this.cursorsPlayer01 = _game.input.keyboard.addKeys( 
-            { 'up': Phaser.KeyCode.W, 
+            { 'up': Phaser.KeyCode.E, 
              'down': Phaser.KeyCode.S, 
              'left': Phaser.KeyCode.A, 
              'right': Phaser.KeyCode.D,
              'hold': Phaser.KeyCode.Q,
-             'rotate': Phaser.KeyCode.E} );
-        //Q = Hold / E = Rotate
+             'rotate': Phaser.KeyCode.W} );
+        //Q = Hold / W = Rotate
 };
  
 tetris.inputManager.prototype = Object.create(tetris.inputManager.prototype);
