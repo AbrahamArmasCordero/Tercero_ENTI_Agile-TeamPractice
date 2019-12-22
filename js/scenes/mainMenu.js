facb00eaf3c57e6069ea5527fbd483361281c032
Abraham Armas Cordero <abraham.armas@enti.cat>
2019-12-22 16:55:01 +0000

    input mapping

--- js/scenes/mainMenu.js
+++ js/scenes/mainMenu.js
@@ -16,6 +16,8 @@ tetris.mainMenu = {
         
         this.load.image("inputText", ruta + 'frameLayout/inputText.png');
         this.load.image("mainBg", ruta + 'backgrounds/MainMenu.png');
+        this.load.image("p1Inputs", ruta+'p1Inputs_img.png');
+        this.load.image("p2Inputs", ruta+'p2Inputs_img.png');
         
         //Musics
          this.game.load.audio('ingameMusic', 'assets/audio/music/tetris_game.mp3');
@@ -85,6 +87,9 @@ tetris.mainMenu = {
             textAlign: 'center',
             cursorColor : '#000000'
         });
+        
+        this.p1InputImg = this.game.add.image(0, 600-30, 'p1Inputs');
+        this.p1InputImg = this.game.add.image(gameOptions.gameWidth-160, 600-30, 'p2Inputs');
         
         
         
